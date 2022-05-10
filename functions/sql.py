63dc70505f57701aa49aeb748b5dce79ba08aefe
FlashLight552 <saw1997hunterone@gmail.com>
2022-05-10 16:42:02 +0300

    update

---
+++ functions/sql.py
@@ -0,0 +1,48 @@
+import sqlite3
+import mariadb
+
+from config.config import MARIA_PORT, MARIA_USER, MARIA_PASSWD, MARIA_HOST, \
+                            MARIA_DB
+
+class Database:
+    def __init__(self,db_file='config/db/piter.sql'):
+        try:
+            self.connection = mariadb.connect(
+                                user=MARIA_USER,
+                                password=MARIA_PASSWD,
+                                host=MARIA_HOST,
+                                port=MARIA_PORT,
+                                database=MARIA_DB
+                                )
+            self.cursor = self.connection.cursor()
+        
+        except:
+            pass
+            # self.connection = sqlite3.connect(db_file)
+            # self.cursor = self.connection.cursor()
+
+
+    def discord_token_crate_table(self):
+        with self.connection:
+            self.cursor.execute("""CREATE TABLE IF NOT EXISTS discord_token (
+                user_id INT PRIMARY KEY,
+                ds_token TEXT 
+                )""")
+            self.connection.commit()
+
+    def discord_token_save(self, user_id, ds_token):
+        self.discord_token_crate_table()
+        
+        with self.connection:
+            self.cursor.execute("""INSERT OR REPLACE INTO discord_token VALUES (?,?)
+                """, (user_id, ds_token, ))
+            self.connection.commit()
+
+    def discord_token_request(self, user_id):
+        self.discord_token_crate_table()
+       
+        with self.connection:
+            result = self.cursor.execute("""SELECT ds_token FROM discord_token WHERE user_id=(?)
+                """, (user_id, )).fetchone()
+            return result[0]    
+            
--- handlers/discord_music.py
+++ handlers/discord_music.py
@@ -2,26 +2,30 @@ from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from config.config import CHAT_ID
 from functions.zenon import zenon
+from functions.sql import Database
 import asyncio
 
-async def discord_token(message:types.Message, state:FSMContext):
-    async with state.proxy() as proxy:
-        proxy['discord_token'] = ds_token = message.text.split()[:14][1].replace('"','')
-        msg = await message.answer(f'Твой Discord token: \n\n{ds_token} \n\nСообщение будет удалено через 10 секунд.')
-        await message.delete()
-        await asyncio.sleep(10)
-        await msg.delete()
+async def discord_token(message:types.Message):
+    ds_token = message.text.split()[:14][1].replace('"','')
+    
+    db = Database()
+    db.discord_token_save(message.from_user.id, ds_token)
+    
+    msg = await message.answer(f'Твой Discord token: \n\n{ds_token} \n\nСообщение будет удалено через 10 секунд.')
+    await message.delete()
+    await asyncio.sleep(10)
+    await msg.delete()
        
 
-async def discord_music_sender(message: types.Message, state:FSMContext):
-    async with state.proxy() as proxy:
-        if 'discord_token' in proxy:
-            token = proxy['discord_token']
-            command = message.text
-            client = zenon.Client(token)
-            client.send_message(CHAT_ID, f'!! {command}')
-        else:
-            await message.answer('Для начала добавь свой Discord token: \n/discord_token "TOKEN"')   
+async def discord_music_sender(message: types.Message):
+    db = Database()
+    token = db.discord_token_request(message.from_user.id)
+    if token:
+        command = message.text
+        client = zenon.Client(token)
+        client.send_message(CHAT_ID, f'!! {command}')
+    else:
+        await message.answer('Для начала добавь свой Discord token: \n/discord_token "TOKEN"')   
     
 
 def discord_handler(dp: Dispatcher):
