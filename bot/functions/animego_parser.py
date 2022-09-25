1e8c78660992580e9b29c24042e5b211768e7537
FlashLight552 <saw1997hunterone@gmail.com>
2022-09-25 11:53:15 +0300

    Update animego_parser.py

--- bot/functions/animego_parser.py
+++ bot/functions/animego_parser.py
@@ -37,36 +37,38 @@ def request_anime_info(url):
 
 async def new_ep_detector_and_send_msg():
     while True:
-        await asyncio.sleep(60 * 15)
-        url = 'https://animego.org/'
-        r = requests.get(url)
-        if r.status_code != 404:
-            soup = bs(r.text, 'lxml')
-            # new = soup.find_all('span', class_='last-update-title font-weight-600')
-            last_update = soup.find('div', class_='last-update')
-            new = last_update.find_all('div', class_='media-body')
-            list = []
+        try:
+            await asyncio.sleep(60 * 15)
+            url = 'https://animego.org/'
+            r = requests.get(url)
+            if r.status_code != 404:
+                soup = bs(r.text, 'lxml')
+                # new = soup.find_all('span', class_='last-update-title font-weight-600')
+                last_update = soup.find('div', class_='last-update')
+                new = last_update.find_all('div', class_='media-body')
+                list = []
 
-            for item in new:
-                title = str(item.span.text).replace('Профессиональный многоголосый', 'Профессиональный')
-                ep = item.find('div', class_='font-weight-600 text-truncate').text
-                studio = str(item.find('div', class_='text-gray-dark-6').text).replace('(','').replace(')','')
-                result = {'title':title, 'studio':studio, 'ep':ep}
-                list.append(result)
+                for item in new:
+                    title = str(item.span.text).replace('Профессиональный многоголосый', 'Профессиональный')
+                    ep = item.find('div', class_='font-weight-600 text-truncate').text
+                    studio = str(item.find('div', class_='text-gray-dark-6').text).replace('(','').replace(')','')
+                    result = {'title':title, 'studio':studio, 'ep':ep}
+                    list.append(result)
 
-            db = Database()
-            with db.connection:
-                for item in list:
-                    if db.select_from_last_anime_update(item['title'], item['ep'], item['studio']):
-                        # print(f'allready have {item}')
-                        pass
-                    else:
-                        # print(f'add {item}')
-                        db.add_last_anime_update(item['title'], item['ep'], item['studio'])
+                db = Database()
+                with db.connection:
+                    for item in list:
+                        if db.select_from_last_anime_update(item['title'], item['ep'], item['studio']):
+                            # print(f'allready have {item}')
+                            pass
+                        else:
+                            # print(f'add {item}')
+                            db.add_last_anime_update(item['title'], item['ep'], item['studio'])
 
-                        user_list = db.select_user_from_sub_to_title(item['title'], item['studio'])
-                        for user in user_list:
-                            text = f"Вышел новый эпизод!\nНазвание: {item['title']}\nЭпизод: {item['ep']}\nСтудия: {item['studio']}"
-                            await bot.send_message(user, text)
-                            # print("Отправил сообщение {user}")
-                            await asyncio.sleep(0.2)
+                            user_list = db.select_user_from_sub_to_title(item['title'], item['studio'])
+                            for user in user_list:
+                                text = f"Вышел новый эпизод!\nНазвание: {item['title']}\nЭпизод: {item['ep']}\nСтудия: {item['studio']}"
+                                await bot.send_message(user, text)
+                                # print("Отправил сообщение {user}")
+                                await asyncio.sleep(0.2)
+        except: pass
